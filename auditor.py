5d512d612c5461215b15ba6c26e959280e402c55
Yap Zheng Yi <2605665@sit.singaporetech.edu.sg>
2026-09-14 21:01:21 +0800

    Requirement 5

--- auditor.py
+++ auditor.py
@@ -3,10 +3,12 @@ stock = ""
 
 while (stock !="quit"):
     stock = input("Enter stock quantity: ").lower()
-    if stock.isdigit():
+    if stock.isdigit():   # only accepts digits, does not recognise "-" sign
         stock = int(stock)
     elif stock == "quit":
         break
+    elif "-" in stock:
+        print("No negative numbers")
     else:
         print("Error")
 
